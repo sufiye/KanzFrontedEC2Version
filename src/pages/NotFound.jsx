4e5c395dd30b16abfd6a3af1b25f513294f4089d
Husey_sz11@itstep.edu.az <Husey_sz11@itstep.edu.az>
2025-12-25 11:47:47 +0400

    all

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useDarkmode } from '../stores/store'
 const NotFound = () => {
+  const { isDarkmodeEnabled } = useDarkmode()
+
     const navigate = useNavigate();
     useEffect(()=>{
         const timer = setTimeout(()=>{
@@ -10,8 +13,17 @@ const NotFound = () => {
         return ()=>clearTimeout(timer);
     },[])
   return (
-    <div  className='w-full h-screen flex justify-center items-center'>
-       <h2 className='text-5xl text-pink-600 font-bold'> Not Found :|</h2>
+    <div  className={`w-full h-screen flex  gap-10 justify-center items-center ${isDarkmodeEnabled ?"bg-[#181A2A]":"bg-white"}`}>
+      <div className='grid grid-cols-1'>
+
+      <div className='grid grid-cols-2 gap-5'>
+      <div className='border-2 h-50 w-50 rounded-full flex justify-center  bg-white'><div className='border h-20 w-20 rounded-full mt-2 bg-black'></div></div>
+      <div className='border-2 h-50 w-50 rounded-full flex justify-center bg-white'><div className='border h-20 w-20 rounded-full mt-2 bg-black'></div></div>
+      </div>
+
+      <div className='w-100 border-2 h-10 rounded-2xl mt-5 bg-white'></div>
+      </div>
+       <h2 className={`text-9xl  font-bold ${isDarkmodeEnabled?"text-white":"text-black"}`}> Not Found</h2>
         </div>
   )
 }

89b926e02c7aafc58999a74cb4d71cad19c97198
Husey_sz11@itstep.edu.az <Husey_sz11@itstep.edu.az>
2026-01-05 16:25:43 +0400

    all

--- src/pages/MyBlogs.jsx
+++ src/pages/MyBlogs.jsx
@@ -9,8 +9,10 @@ const MyBlogs = () => {
   const { isDarkmodeEnabled } = useDarkmode()
   const { id } = useParams()
   const [deleteblog, setDeleteblog] = useState(false)
+  const [visibleCount, setVisibleCount] = useState(10)
   const [blogs, setBlogs] = useState([])
 
+  const visibleBlogs = blogs.slice(0, visibleCount)
 
   const getBlogs = async () => {
     try {
@@ -46,13 +48,19 @@ const MyBlogs = () => {
           <div className={`w-235 h-30 flex justify-center items-center rounded-2xl  mb-10 ${isDarkmodeEnabled ? "bg-slate-800" : "bg-[#F6F6F7]"}`}><a className={`${isDarkmodeEnabled ? "text-white" : "text-black"}`} href="">{blogs[0]?.user?.email}</a></div>
           <div className={`mr-193 text-3xl font-bold mb-10 ${isDarkmodeEnabled ? "text-white" : "text-black"}`}><h1>Latest Post</h1></div>
           <div className="grid grid-cols-3 gap-5 ">
-            {blogs.map(blog => (
-
+            {visibleBlogs.map(blog => (
               <Card key={blog._id} card={blog} deleteblog={deleteblog} setDeleteblog={setDeleteblog} />
             ))}
           </div>
+          {visibleCount < blogs.length && (
+            <button
+              onClick={() => setVisibleCount(prev => prev + 10)}
+              className={` border-2  rounded-lg  h-13 w-33 text-sm mt-10 cursor-pointer
+                ${isDarkmodeEnabled ? "text-zinc-300 border-zinc-400" : "text-[#696A75] border-[#696A754D]/40"}`}>
+              Load more
+            </button>
+          )}
         </div>
-
         <Footer />
       </div >
     </>
